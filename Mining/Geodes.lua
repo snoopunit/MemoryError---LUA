9df53940842affc540fed238e30f8ba742dccedd
snoopunit <31869712+snoopunit@users.noreply.github.com>
2026-08-31 20:34:34 -0400

    Implement geode management script
    This script manages geode inventory and actions, including opening geodes and loading presets when the inventory is full.

---
+++ Mining/Geodes.lua
@@ -0,0 +1,53 @@
+local API = require("api")
+
+local function hasGeodes()
+
+    return Inventory:Contains("Sedimentary geode")
+
+end
+
+local function openGeodes()
+
+  local geodeAB = API.GetABs_name("geode", false)
+
+  if geodeAB.action == "Open" and geodeAB.enabled then
+    return API.DoAction_Ability_Direct(geodeAB, 1, API.OFF_ACT_GeneralInterface_route)
+  end
+
+  return false
+
+end
+
+local function loadLastPreset()
+  return Interact:NPC("Banker", "Load Last Preset from", 10)
+end
+
+
+--Exported function list is in API
+--main loop
+API.Write_LoopyLoop(true)
+API.SetDrawLogs(true)
+API.SetDrawTrackedSkills(true)
+API.SetMaxIdleTime(4)
+while(API.Read_LoopyLoop())
+do-----------------------------------------------------------------------------------
+    if not (API.PlayerLoggedIn()) then
+        print("Player is not logged in. Terminating Script.")
+        API.Write_LoopyLoop(false)
+        return     
+    end
+    
+    if Inventory:IsFull() then
+        loadLastPreset()
+        API.RandomSleep2(600, 0, 600)
+    else
+        if not hasGeodes() then
+            print("Ran out of geodes!")
+            API.Write_LoopyLoop(false)
+        end
+        openGeodes()
+    end
+
+    API.RandomSleep2(10, 0, 50)
+
+end----------------------------------------------------------------------------------
