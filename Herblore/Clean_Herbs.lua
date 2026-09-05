dff2a6f4994d879aa5072b6809925699ebc2d9a1
snoopunit <31869712+snoopunit@users.noreply.github.com>
2026-09-05 16:30:52 -0400

    new clean herbs script

---
+++ Herblore/Clean_Herbs.lua
@@ -0,0 +1,45 @@
+local API = require("api")
+local HERB = require("lib/HERBLORE")
+
+local function loadLastPreset(item) 
+        if not Interact:NPC("Banker", "Load Last Preset from",10) then
+            if not Interact:Object("Bank chest", "Load Last Preset from",10) then
+                API.logWarn("Unable to interact with Banker!")
+                API.Write_LoopyLoop(false)
+            end
+        end
+        API.RandomSleep2(600, 0, 250)
+        return Inventory:Contains(item)
+end
+
+local function cleanHerbs(herbID)
+    if HERB.cleanHerbs(herbID) then
+        MISC.doCrafting()
+    else
+        API.logWarn("Unable to clean herbs!")
+        API.Write_LoopyLoop(false)
+    end
+end
+
+API.Write_LoopyLoop(true)
+API.SetDrawLogs(true)
+API.SetDrawTrackedSkills(true)
+API.SetMaxIdleTime(Max_AFK)
+
+while(API.Read_LoopyLoop())
+
+do-----------------------------------------------------------------------------------
+    if HERB.findGrimyHerbs() then
+        if HERB.cleanHerbs(HERB.findGrimyHerbs().ID) then
+            MISC.doCrafting()
+        end
+    else
+        if not loadLastPreset(HERB.findGrimyHerbs().Name) then
+            API.logWarn("No grimy herbs in inventory!")
+            API.Write_LoopyLoop(false)
+            return false
+        end
+    end
+
+    API.RandomSleep2(800, 0, 400)
+end----------------------------------------------------------------------------------
