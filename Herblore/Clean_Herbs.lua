02dbbe3d737b81eb28c22d0b1691f15d22ca13f9
snoopunit <31869712+snoopunit@users.noreply.github.com>
2026-09-05 16:45:01 -0400

    fix again again

--- Herblore/Clean_Herbs.lua
+++ Herblore/Clean_Herbs.lua
@@ -2,15 +2,14 @@ local API = require("api")
 local HERB = require("lib/HERBLORE")
 local MISC = require("lib/MISC")
 
-local function loadLastPreset(herb) 
-        if not Interact:NPC("Banker", "Load Last Preset from",10) then
-            if not Interact:Object("Bank chest", "Load Last Preset from",10) then
-                API.logWarn("Unable to interact with Banker!")
-                API.Write_LoopyLoop(false)
-            end
+local function loadLastPreset() 
+    if not Interact:NPC("Banker", "Load Last Preset from",10) then
+        if not Interact:Object("Bank chest", "Load Last Preset from",10) then
+            API.logWarn("Unable to interact with Banker!")
+            API.Write_LoopyLoop(false)
         end
-        API.RandomSleep2(600, 0, 250)
-        return Inventory:Contains(herb.Name)
+    end
+    return Inventory:IsFull()
 end
 
 local function main()
@@ -22,8 +21,8 @@ local function main()
             MISC.doCrafting()
         end
     else
-        if not loadLastPreset(herb) then
-            API.logWarn("No grimy herbs in inventory!")
+        if not loadLastPreset() then
+            API.logWarn("Failed to load a full inventory!")
             API.Write_LoopyLoop(false)
             return false
         end
