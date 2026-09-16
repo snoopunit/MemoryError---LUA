4fb58d8a99a0cd298ba96de6b3d97dcfa7436d48
snoopunit <snoopunit@gmail.com>
2026-09-16 14:24:18 -0500

    fix

--- Lib/MISC.lua
+++ Lib/MISC.lua
@@ -61,7 +61,8 @@ function Miscellaneous.isCraftingInterfaceOpen()
 end
 
 function Miscellaneous.isChooseToolOpen()
-    return API.VB_FindPSettinOrder(2874).state == 1277970 
+    --return API.VB_FindPSettinOrder(2874).state == 1277970 
+    return API.ScanForInterfaceTest2Get2(false, { 1179, 99, 14, 0 })[1].textids == "Choose a tool"
 end
 
 function Miscellaneous.isSwitchToolMenuOpen()
