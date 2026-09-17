e6f36a644b2dda03cb45281ff174f618ad00774d
snoopunit <snoopunit@gmail.com>
2026-09-17 12:29:06 -0500

    ok

--- Template.lua
+++ Template.lua
@@ -59,17 +59,17 @@ while(API.Read_LoopyLoop())
 
 do-----------------------------------------------------------------------------------
 
-    --[[if Inventory:Contains("Impious ashes") and Inventory:Contains("Willow incense sticks") then
+    if Inventory:Contains("Accursed ashes") and Inventory:Contains("Maple incense sticks") then
         coatIncense() 
     else
-        if not loadLastPreset("Impious ashes") then
-            API.logWarn("No Impious ashes in inventory!")
+        if not loadLastPreset("Accursed ashes") then
+            API.logWarn("No Accursed ashes in inventory!")
             API.Write_LoopyLoop(false)
             return false
         end
-    end]]
+    end
 
-    if Inventory:Contains("Maple logs") then
+    --[[if Inventory:Contains("Maple logs") then
         craftIncense() 
     else
         if not loadLastPreset("Maple logs") then
@@ -77,7 +77,7 @@ do------------------------------------------------------------------------------
             API.Write_LoopyLoop(false)
             return false
         end
-    end
+    end]]
 
   
 
