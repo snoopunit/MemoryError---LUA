65b9c782a984d6ca9ecfa4a60912cf3712388d81
snoopunit <31869712+snoopunit@users.noreply.github.com>
2026-09-18 12:24:15 -0400

    Reintroduce print statements and functions in Template.lua

--- Template.lua
+++ Template.lua
@@ -55,6 +55,7 @@ local function readChat()
 
     for index, value in ipairs(chats) do
         if value.text then
+            API.logDebug("Chat: "..value.text)
             return value.text
         end
     end   
